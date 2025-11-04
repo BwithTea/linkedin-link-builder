bf993f523bf3ab65051a25deb0d10497b166bdef
bwithtea <bwithtea@gmail.com>
2025-11-04 05:04:55 -0500

    feat: Add location input field and enhance styling for Link Builder components

--- link-builder/src/components/Pages/LinkBuilder.jsx
+++ link-builder/src/components/Pages/LinkBuilder.jsx
@@ -8,12 +8,13 @@ import { useState } from 'react'
 export default function LinkBuilder() {
     const [jobTitle, setJob] = useState("");
     const [time, setTime] = useState("");
+    const [location, setLocation] = useState("");
     
     return (
         <div className="Page-Container">
             <h1 className="app-title">LinkedIn Link Builder</h1>
             <Card>
-                <LinkGen jobTitle={jobTitle} setJob={setJob} time={time} setTime={setTime} />
+                <LinkGen jobTitle={jobTitle} setJob={setJob} time={time} setTime={setTime} location={location} setLocation={setLocation} />
                 <Button type="button" variant="final"> Generate Link</Button>
                 <Input id="generated-link" type="text" value={`https://www.linkedin.com/jobs/search/?f_TPR=r${time}&keywords=${encodeURIComponent(jobTitle)}`} readOnly />
             </Card>
--- link-builder/src/components/organisms/LinkGen.jsx
+++ link-builder/src/components/organisms/LinkGen.jsx
@@ -2,7 +2,7 @@ import InputField from '../molecules/inputField.jsx'
 import SelectField from '../molecules/selectField.jsx'
 import FormField from '../molecules/FormField.jsx'
 
-export default function LinkGen({ time, setTime, jobTitle, setJob }) {
+export default function LinkGen({ time, setTime, jobTitle, setJob, location, setLocation }) {
     return (
         <div>
             <FormField>
@@ -10,8 +10,13 @@ export default function LinkGen({ time, setTime, jobTitle, setJob }) {
             </FormField>
             
             <FormField>
+                <InputField id="location" label="Location" type="text" placeholder="Enter location" value={location} onChange={setLocation} />
+            </FormField>
+
+            <FormField>
                 <SelectField id="time" label="Time" value={time} onChange={setTime} />
             </FormField>
+
         </div>
         
     )
